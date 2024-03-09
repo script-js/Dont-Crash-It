054c4d475095fee93692b6471f57f1a262c3219f
script-js <134544514+script-js@users.noreply.github.com>
2024-03-09 09:35:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function createOB() {
+  var newob = document.createElement("img")
+  var array = ["src/obstacles/"]
+  newob.src = array[Math.floor(Math.random() * array.length)]
+  newob.id = "ob" + Math.ceil(Math.floor(Math.random() * 1000))
+  newob.width = "100px";
+  obstacles.appendChild(newob)
+  var elem = document.getElementById(newob.id)
+  elem.interval = setInterval(function() {
+    elem.width = (parseInt(elem.width) + 1) + "px";
+    console.log(elem.width)
+    if (elem.width > 600) {
+      elem.remove()
+      clearInterval(elem.interval)
+    }
+  },1)
+}
