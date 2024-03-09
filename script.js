696649b3db0feb8f94916d30c9c245001e2f4eb4
script-js <134544514+script-js@users.noreply.github.com>
2024-03-09 09:40:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ function createOB() {
   newob.src = array[Math.floor(Math.random() * array.length)]
   newob.id = "ob" + Math.ceil(Math.floor(Math.random() * 1000))
   newob.width = "100";
+  newOB.classList = "obstacle";
   obstacles.appendChild(newob)
   var elem = document.getElementById(newob.id)
   elem.interval = setInterval(function() {
     elem.width = parseInt(elem.width) + 1;
-    console.log(elem.width)
+    console.log(elem.interval)
     if (elem.width > 600) {
       elem.remove()
       clearInterval(elem.interval)
