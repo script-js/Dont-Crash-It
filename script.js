ba19a512a4456adeda0a55955a4d3e5600dbcfad
script-js <134544514+script-js@users.noreply.github.com>
2024-03-10 14:16:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var moveint;
+var speed = 50;
 
 function createOB() {
   var newob = document.createElement("img")
@@ -6,8 +7,10 @@ function createOB() {
   newob.src = array[Math.floor(Math.random() * array.length)]
   newob.id = "ob" + Math.ceil(Math.floor(Math.random() * 1000))
   newob.width = "100";
+  newob.style.left Math.floor(Math.random() * )
   newob.classList = "obstacle";
   obstacles.appendChild(newob)
+  speed = speed - 1
   var elem = document.getElementById(newob.id)
   elem.interval = setInterval(function() {
     elem.width = parseInt(elem.width) + 1;
@@ -16,7 +19,7 @@ function createOB() {
       elem.remove()
       clearInterval(elem.interval)
     }
-  },1)
+  },speed)
 }
 
 function viewMove(dir) {
@@ -24,11 +27,14 @@ function viewMove(dir) {
     var elemList = obstacles.querySelector(".obstacle")
       Object.keys(elemList).forEach(function (k) {
         if (dir == "l") {
-      var nval = parseInt(elemList[k].style.left) + 1
-    } else if (dir == "r") {
-      var nval = parseInt(elemList[k].style.left) - 1
-    }
+          var nval = parseInt(elemList[k].style.left) + 1
+        } else if (dir == "r") {
+          var nval = parseInt(elemList[k].style.left) - 1
+        }
         elemList[k].style.left = nval + "px";
+        if (parseInt(elemList[k].style.left) > (innerHeight - 200) || parseInt(elemList[k].style.left) < 1) {
+          elemList[k].remove()
+        }
       })
   },1)
 }
